771f0c0de480443e091d2a94cdc78c584a6e0640
senohirona <seno.hirona@tsunolab.net>
2020-11-26 13:37:03 +0900

    complete chapter4-5

--- taskleaf/app/controllers/admin/users_controller.rb
+++ taskleaf/app/controllers/admin/users_controller.rb
@@ -1,5 +1,7 @@
 class Admin::UsersController < ApplicationController
 
+  before_action :require_admin
+
   def index
     @users = User.all
   end
@@ -48,4 +50,7 @@ class Admin::UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 
+  def require_admin
+    redirect_to root_path unless current_user.admin
+  end
 end
